db2e004cab57668f2a4988543fab11f794d57c2f
Bruno Ramos <125314540+oBrunoRamos@users.noreply.github.com>
2024-07-12 17:07:30 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+       <div id="app">
+              <nav id="nav">
+                     <router-link to="/">Home</router-link>
+              </nav>
+              <router-view></router-view>
+       </div>
+</template>
+
+<script>
+export default {
+       name:"App"
+
+}
+</script>
+
+<style>
+
+</style>
+</style>
